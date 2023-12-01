13230c858bce821de4fe268ff82fc1b63f04d0ca
DeDMaKSiM1 <152630416+DeDMaKSiM1@users.noreply.github.com>
2023-12-01 21:51:15 +0300

    Init project commit
    Загрузил проект на GitHub

---
+++ MyPlatform/Assets/Scripts/Hero.cs
@@ -0,0 +1,57 @@
+
+using UnityEngine;
+namespace MyPlatform {
+    public class Hero : MonoBehaviour
+    {
+        //Ќужно воплотить вертикальное передвижение, без физики в одном методе
+        [SerializeField] private float _speed;
+        [SerializeField] private float _jumpSpeed;
+
+        [SerializeField] private LayerCheck _groundCheck;
+
+        Vector2 _direction;
+        private Rigidbody2D rbody;
+        private void Awake()
+        {
+            rbody = GetComponent<Rigidbody2D>();    
+        }
+        public void SetDirection(Vector2 direction)
+        {
+            _direction = direction;
+        } // ѕринимающий данные о Input метод
+
+        private void FixedUpdate()
+        {
+            rbody.velocity = new Vector2(_direction.x * _speed, rbody.velocity.y) ;
+
+            var isJumping = _direction.y > 0;
+            if (isJumping)
+            {
+                if (IsGrounded())
+                {
+                    rbody.AddForce(Vector2.up * _jumpSpeed, ForceMode2D.Impulse);
+                }
+            }
+            else if (rbody.velocity.y > 0)
+            {
+                rbody.velocity = new Vector2(rbody.velocity.x, rbody.velocity.y * 0.5f);
+            }
+           
+        }
+
+        private bool IsGrounded()
+        {
+            return _groundCheck.IsTouchingLayer ;
+        }
+        public void OnDrawGizmos()
+        {
+            Gizmos.color = IsGrounded() ? Color.green : Color.red;
+        }
+
+        public void Saying()
+        {
+            Debug.Log(message: "Aaaargh");
+        }
+    }
+}
+
---
+++ MyPlatform/Assets/Scripts/HeroInputReader.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+namespace MyPlatform
+{    public class HeroInputReader : MonoBehaviour
+    {
+        [SerializeField] private Hero _hero;
+       
+        private void OnHorizontalMovement(InputAction.CallbackContext context)
+        {
+            var direction = context.ReadValue<Vector2>();
+            _hero.SetDirection(direction);
+        }
+        private void OnClicked(InputAction.CallbackContext context)
+        {
+            if (context.canceled)
+            {
+                _hero.Saying();
+            }
+
+        }
+
+    }
+}
+
