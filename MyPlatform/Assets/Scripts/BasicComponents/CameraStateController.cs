054565c1f8d06b1a5f293d1d6a3c3e4a6a406c9d
DeDMaKSiM1 <152630416+DeDMaKSiM1@users.noreply.github.com>
2024-02-04 23:04:02 +0300

    CutScene added
    Но не доделал

---
+++ MyPlatform/Assets/Scripts/BasicComponents/CameraStateController.cs
@@ -0,0 +1,27 @@
+using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MyPlatform.Components
+{
+    public class CameraStateController : MonoBehaviour 
+    {
+        [SerializeField] private Animator _animator;
+        [SerializeField] private CinemachineVirtualCamera _camera;
+
+        public static readonly int ShowTargetKey = Animator.StringToHash("ShowTarget");
+
+        public void SetPosition(Vector3 targetPosition)
+        {
+            targetPosition.z = _camera.transform.position.z;
+            _camera.transform.position = targetPosition;
+        }
+        public void SetState(bool state)
+        {
+            _animator.SetBool(ShowTargetKey, state);
+        }
+
+    }
+}
+
