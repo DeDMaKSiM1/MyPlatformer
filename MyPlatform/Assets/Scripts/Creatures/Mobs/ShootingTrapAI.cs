88cdb80202475e5dd5bfb549efeca142887a3ac1
DeDMaKSiM1 <152630416+DeDMaKSiM1@users.noreply.github.com>
2024-02-20 21:36:49 +0300

    Доделал ракушку + сделал останки бочки

--- MyPlatform/Assets/Scripts/Creatures/Mobs/ShootingTrapAI.cs
+++ MyPlatform/Assets/Scripts/Creatures/Mobs/ShootingTrapAI.cs
@@ -20,6 +20,17 @@ namespace MyPlatform.Creatures.Mobs
         [SerializeField] private Cooldown _rangeCooldown;
         [SerializeField] private SpawnComponent _rangeAttack;
 
+        private Animator _animator;
+
+        private static readonly int RangeKey = Animator.StringToHash("range");
+        private static readonly int MeleeKey = Animator.StringToHash("melee");
+
+
+        private void Awake()
+        {
+            _animator = GetComponent<Animator>();
+        }
+
         private void Update()
         {
             if (_vision.IsTouchingLayer)
@@ -27,13 +38,37 @@ namespace MyPlatform.Creatures.Mobs
                 if (_meleeCanAttack.IsTouchingLayer)
                 {
                     if (_meleeCooldown.IsReady)
-                    {
                         MeleeAttack();
-                    }
+                    return;
+                }
+
+                if (_rangeCooldown.IsReady)
+                {
+                    RangeAttack();
                 }
             }
         }
 
+        private void MeleeAttack()
+        {
+            _meleeCooldown.Reset();
+            _animator.SetTrigger(MeleeKey);
+        }
+
+        private void RangeAttack()
+        {
+            _rangeCooldown.Reset();
+            _animator.SetTrigger(RangeKey);
+        }
+ 
+        public void OnMeleeAttack()
+        {
+            _meleeAttack.Check();
+        }
+        public void OnRangeAttack()
+        {
+            _rangeAttack.Spawn();
+        }
 
     }
 }
