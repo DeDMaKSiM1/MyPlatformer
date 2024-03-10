86ded363c8f1fd432a81add404c30c1385a61a75
DeDMaKSiM1 <152630416+DeDMaKSiM1@users.noreply.github.com>
2024-03-10 22:52:51 +0300

    UI menu alpha done

---
+++ MyPlatform/Assets/Scripts/UI/Widgets/Editor/CustomButtonEditor.cs
@@ -0,0 +1,23 @@
+using UnityEditor;
+using UnityEditor.UI;
+
+
+namespace MyPlatform.UI.Widgets.Editor
+{
+
+    [CanEditMultipleObjects]
+    [CustomEditor(typeof(CustomButton), true)]
+    public class CustomButtonEditor : ButtonEditor
+    {
+        public override void OnInspectorGUI()
+        {
+
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("_normal"));
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("_pressed"));
+            serializedObject.ApplyModifiedProperties();
+
+            base.OnInspectorGUI();
+        }
+    }
+}
+
