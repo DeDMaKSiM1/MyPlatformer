0787ec0c79ff186d7438f63c1129386690859505
DeDMaKSiM1 <152630416+DeDMaKSiM1@users.noreply.github.com>
2024-03-15 17:02:08 +0300

    15.03

--- MyPlatform/Assets/Scripts/UI/Settings/SettingsWindow.cs
+++ MyPlatform/Assets/Scripts/UI/Settings/SettingsWindow.cs
@@ -1,17 +1,22 @@
 using MyPlatform.Model.Data;
-using System.Collections;
-using System.Collections.Generic;
+using MyPlatform.UI.Widgets;
 using UnityEngine;
 
 namespace MyPlatform.UI.Settings
 {
     public class SettingsWindow : AnimatedWindow
     {
-        //protected override void Start()
-        //{
-        //    base.Start();
-        //    GameSettings.Instance.Music;
-        //}
+
+        [SerializeField] private AudioSettingsWidget _music;
+        [SerializeField] private AudioSettingsWidget _sfx;
+
+        protected override void Start()
+        {
+            base.Start();
+
+            _music.SetModel(GameSettings.Instance.Music);
+            _sfx.SetModel(GameSettings.Instance.Sfx);
+        }
     }
 }
 
---
+++ MyPlatform/Assets/Scripts/UI/Widgets/AudioSettingsWidget.cs
@@ -0,0 +1,45 @@
+using MyPlatform.Model.Data.Properties;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace MyPlatform.UI.Widgets
+{
+    public class AudioSettingsWidget : MonoBehaviour
+    {
+        [SerializeField] private Slider _slider;
+        [SerializeField] private Text _value;
+
+
+        private FloatPersistentProperty _model;
+        private void Start()
+        {
+            _slider.onValueChanged.AddListener(OnSliderValueChanged);
+        }
+
+        private void OnSliderValueChanged(float value)
+        {
+            _model.Value = value;
+        }
+        public void SetModel(FloatPersistentProperty model)
+        {
+            _model = model;
+            model.OnChanged += OnValueChanged;
+            OnValueChanged(model.Value, model.Value);
+        }
+
+        private void OnValueChanged(float newValue, float oldValue)
+        {
+            var textValue = Mathf.Round(newValue * 100);
+            _value.text = textValue.ToString();
+            _slider.normalizedValue = newValue;
+        }
+        private void OnDestroy()
+        {
+            _slider.onValueChanged.RemoveListener(OnSliderValueChanged);
+            _model.OnChanged -= OnValueChanged;
+        }
+    }
+}
+
